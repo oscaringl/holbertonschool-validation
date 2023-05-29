ce98b5c398a813c209f5055c4bb0fb110ffe079b
Oscar Guerra <oogl29@gmail.com>
2023-05-28 20:50:21 -0500

    Update Folder module3_task2

--- module3_task2/content/welcome.md
+++
@@ -1,9 +0,0 @@
----
-title: "Welcome"
-date: 2023-05-02T17:08:15+02:00
-draft: true
----
-
-Enter your name below and click on the button "Say Hello":
-
-{{< sayHello >}}
--- module3_task2/setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-apt-get update && apt-get install -y make curl
-curl -LO https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.tar.gz
-tar -xzf hugo_extended_0.84.0_Linux-64bit.tar.gz -C /usr/local/bin/
-rm hugo_extended_0.84.0_Linux-64bit.tar.gz
-make build
